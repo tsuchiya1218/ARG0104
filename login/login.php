278d64af30b36d653c3eda5bcff00b800696343b
20jy0110 <20jy0110@jec.ac.jp>
2022-02-14 15:55:31 +0900

    ログイン機能の追加

--- login/login.php
+++ login/login.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<title>ARAGIN弁当 ログインページ</title>
-	<link rel="stylesheet" href="/css/common.css">
-</head>
+
+<?php include("../commons/head.php") ?>
+
+
 <body>
-	<div class="wrap">
-	<?php include("../commons/header.php") ?>
-	<main>
-	</main>
-	<?php include("../commons/footer.html")?>
-	</div>
+  <div class="wrap">
+    <?php include("../commons/header.php") ?>
+    <main>
+      <form action="" method="" name="">
+        <h2>ログイン</h2>
+        <hr><br>
+
+        <span id="span1">
+          <p>ユーザーid:</p>
+        </span><input type="text" id="id" class="" maxlength="8" /><br>
+        <span id="span2">
+          <p>パスワード:</p>
+        </span>
+        <input type="password" id="password" class="password">
+        <button onclick="isHanEisu('id','password')">ログイン</button>
+      </form>
+      <script src="../js/login.js"></script>
+    </main>
+    <?php include("../commons/footer.html") ?>
+  </div>
 </body>
+
 </html>
