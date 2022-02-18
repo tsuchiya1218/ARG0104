cec0e23886d9bda8348695c525ae2a907e34cb09
20jy0110 <20jy0110@jec.ac.jp>
2022-02-18 16:37:26 +0900

    ログイン機能の改善

---
+++ account/dbaccess.php
@@ -0,0 +1,42 @@
+<?php
+// ログイン処理
+function login($email, $password)
+{
+  $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+  $db->query('SET NAMES utf8');
+  $sql = "SELECT *  FROM accounts  WHERE email = :email AND  password = :password";
+  $stt = $db->prepare($sql);
+  $stt->bindParam(':email', $email);
+  $stt->bindParam(':password', $password);
+  $stt->execute();
+  while ($row = $stt->fetch()) {
+    $result['id'] = $row['id'];
+    $result['name'] = $row['name'];
+    $result['email'] = $row['email'];
+    $result['password'] = $row['password'];
+  }
+  if (isset($result)) {
+    return $result;
+  }
+}
+// ログイン認証
+function authCheck($email, $password)
+{
+  $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+  $db->query('SET NAMES utf8');
+  $sql = "SELECT * FROM accounts WHERE email = :email AND password = :password ";
+  $stt = $db->prepare($sql);
+  $stt->bindParam(':email', $email);
+  $stt->bindParam(':password', $password);
+  $stt->execute();
+  while ($row = $stt->fetch()) {
+    $result['id'] = $row['id'];
+    $result['name'] = $row['name'];
+    $result['email'] = $row['email'];
+    $result['password'] = $row['password'];
+  }
+  if (isset($result)) {
+    return $result;
+  }
+}
+?>
