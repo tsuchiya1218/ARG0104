da936b7f054d00e85e845e209281816bf3b74c45
20jy0110 <20jy0110@jec.ac.jp>
2022-03-04 16:03:49 +0900

    フォームの変更

--- userEntry/userRegister.php
+++ userEntry/userRegister.php
@@ -15,19 +15,36 @@
 	<main>
 		<div class="container py-4" id="contact">
 			<form action="userEntry.php" method="post" name="user">
-				<h2>新規会員登録</h2>
-				<p>以下のフォームに必要情報を入力してください。</p>
-				<p>メールアドレス：<input type="email" name="email" placeholder="example@xxx.com" required></p>
-				<p>パスワード(半角英数字6~8文字)：<input type="password" name="password" id="password" maxlength="8" required></p>
-				<p>氏名(全角カナ)：<input type="text" name="name" required></p>
-				<p>住所：<input type="text" name="address" required></p>
-				<p>電話番号：<input type="tel" name="tel" required></p>
-				<p>支払い方法：<input type="radio" id="credit" name="payment" value="" required>
-					<label for="credit">クレジット決済</label>
-					<input type="radio" id="cash" name="payment" value="">
-					<label for="cash">現金</label>
-					<input type="submit" onclick="check()" class="clickbtn btn" value="登録">
-					</from>
+				<table>
+					<h2>新規会員登録</h2>
+					<p>以下のフォームに必要情報を入力してください。</p>
+					<tr>
+						<th><label for="exampleInputEmail1" class="form-label">メールアドレス：</label></th>
+						<td><input type="email" name="email" placeholder="example@xxx.com" required></td>
+					</tr>
+					<tr>
+						<th><label for="exampleInputPassword1" class="form-label">パスワード(半角英数字6~8文字)</label></th>
+						<td><input type="password" name="password" id="password" maxlength="8" required></td>
+					</tr>
+					<tr>
+						<th><label for="exampleInputName" class="form-label">氏名(全角カナ)：</label></th>
+						<td><input type="text" name="name" required></td>
+					</tr>
+					<tr>
+						<th><label for="exampleInputNumber" class="form-label">電話番号：</label></th>
+						<td><input type="tel" name="tel" required></td>
+					</tr>
+					<tr>
+						<th>支払い方法：</th>
+						<td><input type="radio" id="credit" name="payment" value="" required>
+							<label for="credit">クレジット決済</label>
+							<input type="radio" id="cash" name="payment" value="">
+							<label for="cash">現金</label>
+							<input type="submit" onclick="check()" class="clickbtn btn" value="登録">
+							</from>
+						</td>
+					</tr>
+				</table>
 		</div>
 	</main>
 	<footer>
@@ -35,6 +52,6 @@
 	</footer>
 
 </body>
-<script src="../formCheack.js"></script>
+<script src="./formCheck.js"></script>
 
 </html>
